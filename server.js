dd01b62102de7b7342c20ded943c2857feff0080
Anisa-barvin <barvinanisa@gmail.com>
2026-09-18 09:18:02 +0530

    Update application message

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from Jenkins Anisa Barvin A!");
+  res.end("Hello from Jenkins CI/CD - Version 2!");
 });
 
 server.listen(3000, () => {
